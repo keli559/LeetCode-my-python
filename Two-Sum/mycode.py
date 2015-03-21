8a6a1398bdcd71b5dc371608b8daa3ee0533b178
Ke Li <likekeustc@hotmail.com>
2015-03-20 20:11:35 -0400

    twosum

--- Two-Sum/mycode.py
+++ Two-Sum/mycode.py
@@ -1,12 +1,24 @@
 class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
-        if num == []:
-            return (-1, -1)
-        dict = {}
+        # Function takes in a list of numbers, 'num'
+        # and the sum value 'target'
+        # Function returns the two indices in list 'num'
+         # Index1 and Index2
+         if num == []:
+             return (-1, -1)
+         dict = {}
+         # dictionary /hash table is structured
+         #  subtraction(can be negative):current index
+         # target - num[ii]: ii
+         #
+         # Although there are negatives for (target -num[ii])
+         # but pick only when the current num[ii] is in the dictionary.keys()
+         # Therefore, the negative keys are automatically filtered! Excellent!
         m = len(num)
         for ii in range(m):
             if num[ii] in dict.keys():
+                
                 return (dict[num[ii]]+1, ii+1)
             else:
                 tmp = target-num[ii]
